cbf06426e327b3381b8ed4c953bb505b42d5cfec
John <johndavidchancey@icloud.com>
2019-11-26 03:11:16 -0500

    Last fix on say

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,7 @@ exports.run = async(client, message, args) => {
     
 	if (message.member.roles.some(role => role.name === 'Moderator')) {
 		
-		message.delete(1000)
+		message.delete(500)
 			
 			if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
@@ -22,25 +22,6 @@ exports.run = async(client, message, args) => {
 			  message.channel.send(args.join(" "));
 	}
 
-	if (message.member.roles.some(role => role.name === 'Admin')) {
-			
-			message.delete(1000)
-				
-				if (!args.length) 
-				return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-				
-	
-				const roleColor = message.guild.me.highestRole.hexColor;
 
-				if (args[0].toLowerCase() === "embed") {
-				  const embed = new RichEmbed()
-					.setDescription(args.slice(1).join(" "))
-					.setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
-			
-				  message.channel.send(embed);
-				} else {
-				  message.channel.send(args.join(" "));
-	}
-}
 }
 }

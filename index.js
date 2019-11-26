a4c25a6202ed7a23a78bc13879142897e90a55b7
John <johndavidchancey@icloud.com>
2019-11-26 00:01:35 -0500

    Made Config file for storing bot Token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
 const fs = require("fs");
+const config = require('./config.json');
 
 
 client.commands = new Discord.Collection();
@@ -31,4 +32,4 @@ fs.readdir("./commands/", (err, files) => {
 
 
 client.on("ready", () => console.log("Online!"));
-client.login("NjQyNDk5MDMzODY4NDAyNzAz.Xdx2kg.4etx4Q7c4hN1nXFzRPAJr6TPmI8")
+client.login(config.token)

078c6d8f973d5c8e4d96a11059ec4bfca4cb2bdc
John <johndavidchancey@icloud.com>
2019-11-26 10:23:46 -0500

    Devs Only

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,10 +1,13 @@
 
 exports.run = async(client, message, args) => {
 	
+	let devs = ["227600936061763604", "283299188697989120"];
 
-   if (!args.length) {
+		if(devs.indexOf(message.author.id) != -1) {
+   		if (!args.length) {
 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 	}
 	client.user.setGame(`${args[0]}`)
+}	
 }
-//Need to make this an owner only command
+

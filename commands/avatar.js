b3d91083499f3ba64839eb201eeef422ec475f29
Kekulism <33201764+Kekulism@users.noreply.github.com>
2019-11-28 15:22:47 -0500

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js")
+
+exports.run = async(client, message, args) => {
+    const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(user.username)
+        .setImage(user.avatarURL);
+    message.channel.send(avatarEmbed);
+};

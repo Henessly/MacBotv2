6cf69925d7e00e893e869cfe50e03b057ba5f017
Kekulism <33201764+Kekulism@users.noreply.github.com>
2019-12-03 13:52:05 -0500

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,120 +1,122 @@
 const Discord = require("discord.js");
 
 exports.run = async (reaction, user) => {
+  let furry = reaction.guild.roles.find(`name`, "Furry");
+  let gamer = reaction.guild.roles.find(`name`, "Gamer");
+  let memer = reaction.guild.roles.find(`name`, "Memer");
+  let anime = reaction.guild.roles.find(`name`, "Anime");
+  let ussr = reaction.guild.roles.find(`name`, "Communist");
+  let human = reaction.guild.roles.find(`name`, "Human");
+  let ghost = reaction.guild.roles.find(`name`, "Ghost");
+  
+  let red = reaction.guild.roles.find(`name`, "Red");
+  let orange = reaction.guild.roles.find(`name`, "Orange");
+  let yellow = reaction.guild.roles.find(`name`, "Yellow");
+  let green = reaction.guild.roles.find(`name`, "Green");
+  let blue = reaction.guild.roles.find(`name`, "Blue");
+  let purple = reaction.guild.roles.find(`name`, "Purple");
+  let pink = reaction.guild.roles.find(`name`, "Pink");
+  let white = reaction.guild.roles.find(`name`, "White");
+  let black = reaction.guild.roles.find(`name`, "Black");
+  
   if (!user) return;
   if (user.bot) return;
   if (!reaction.message.channel.guild) return;
   if (reaction.emoji.name == "🦊") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Furry");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(furry)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🎮") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Gamer");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(gamer)
       .catch(console.error);
   }
   if (reaction.emoji.id == "645369444130095135") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Memer");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(memer)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🇯🇵") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Anime");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(anime)
       .catch(console.error);
   }
   if (reaction.emoji.id == "632788377619660802") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Communist");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(ussr)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🙂") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Human");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(human)
       .catch(console.error);
   }
   if (reaction.emoji.name == "👻") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Ghost");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(ghost)
       .catch(console.error);
   }
   //Colors
   if (reaction.emoji.name == "🔴") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Red");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(red)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🟠") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Orange");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(orange)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🟡") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Yellow");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(yellow)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🟢") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Green");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(green)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🔵") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Blue");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(blue)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🟣") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Purple");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(purple)
       .catch(console.error);
   }
   if (reaction.emoji.name == "🌸") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Pink");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(pink)
       .catch(console.error);
   }
   if (reaction.emoji.name == "⚪") {
-    let role = reaction.message.guild.roles.find(r => r.name == "White");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(white)
       .catch(console.error);
   }
   if (reaction.emoji.name == "⚫") {
-    let role = reaction.message.guild.roles.find(r => r.name == "Black");
     reaction.message.guild
       .member(user)
-      .addRole(role)
+      .addRole(black)
       .catch(console.error);
   }
 };

2a7ed2ea0405bd5da7a92345e3a9ae655d107735
Kekulism <kekulism@gmail.com>
2019-12-07 13:06:33 -0500

    Added 8ball command

---
+++ commands/8ball.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+
+exports.run = async(client, message, args) => {
+    let results = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and try again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful.",
+        ""
+    ]
+    let result = results[Math.floor(Math.random() * results.length - 1)];
+
+    message.channel.send("🎱 " + result)
+}

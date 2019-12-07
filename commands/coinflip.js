7707746101427e5120bdd2b6008019daaebc9f2f
Kekulism <kekulism@gmail.com>
2019-12-06 21:00:25 -0500

    Bug fix

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 exports.run = async(client, message, args) => {
-    let coins = "Tails, Heads";
+    let coins = ["Tails", "Heads"]
     let coin = coins[Math.floor(Math.random() * coins.length - 1)];
 
     message.channel.send("The coin landed on " + coins + "!")

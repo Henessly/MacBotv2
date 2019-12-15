641e414cf252e29421cc5d9e95b724db838c477f
Zachary Damato <zacharyd1@live.com>
2019-12-14 23:25:35 -0500

    tidy: removed a redundant 'bot online' message

--- modules/setActivity.ts
+++ modules/setActivity.ts
@@ -2,8 +2,6 @@ import {client} from "../Discord-Bot-Core/bot"
 
 //Activity
 client.on("ready", () => {
-    console.log("Online!");
-
   client.user.setActivity("your data", {
     type: "STREAMING",
     url: "https://www.twitch.tv/macplaystv"

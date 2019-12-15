15de614fb4dc4bb966f0747131f420f02a5ea8f1
Zachary Damato <zacharyd1@live.com>
2019-12-15 03:15:49 -0500

    feat: converted the d20 command

---
+++ handlers/default/prefix/d20.ts
@@ -0,0 +1,15 @@
+import * as Discord from "discord.js"
+
+module.exports = {
+    name: 'd20',
+    description: '',
+    aliases: [],
+    //Comment out permissions/channel/server requirements if you want it to run everywhere/by everyone/etc
+    // permissions: [],
+    // inChannelID: [],
+    // inChannelName: [],
+    // inServerID: [],
+    async execute(msg : Discord.Message, args : Array<string>) {
+        msg.channel.send("You rolled a " + (Math.round(Math.random() * 19) + 1) + "!");
+    }
+}

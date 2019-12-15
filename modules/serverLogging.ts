56479d838bcb95f07e947987141a625a52259ea1
Zachary Damato <zacharyd1@live.com>
2019-12-15 00:26:40 -0500

    feat: added try/catch blocks around the ((VERY BUGGY)) server logging code

--- modules/serverLogging.ts
+++ modules/serverLogging.ts
@@ -9,52 +9,80 @@ function getModChannel(guild : Discord.Guild) : Discord.TextChannel
 
 // Deleted Message Log
 client.on("messageDelete", async msg => {
-    let logs = await msg.guild.fetchAuditLogs({type: 72});
-    let entry = logs.entries.first();
-  
-    let embed = new Discord.RichEmbed()
-        .setTitle("**DELETED MESSAGE**")
-        .setColor("#fc3c3c")
-        .addField("Author", msg.author.tag, true)
-        .addField("Channel", msg.channel, true)
-        .addField("Message", msg.content)
-        .setFooter(`Message ID: ${msg.id} | Author ID: ${msg.author.id}`);
-  
-    getModChannel(msg.guild).send({embed});
+    try
+    {
+        let logs = await msg.guild.fetchAuditLogs({type: 72});
+        let entry = logs.entries.first();
+    
+        let embed = new Discord.RichEmbed()
+            .setTitle("**DELETED MESSAGE**")
+            .setColor("#fc3c3c")
+            .addField("Author", msg.author.tag, true)
+            .addField("Channel", msg.channel, true)
+            .addField("Message", msg.content)
+            .setFooter(`Message ID: ${msg.id} | Author ID: ${msg.author.id}`);
+    
+        getModChannel(msg.guild).send({embed});
+    }
+    catch(e)
+    {
+        console.warn("ERROR: " + e);
+    }
 });
 
   // Edited Message Log
 client.on("messageUpdate", async (oldMessage, newMessage) => {
-    let embed = new Discord.RichEmbed()
-        .setTitle("**EDITED MESSAGE**")
-        .setColor("#fc3c3c")
-        .addField("Author", newMessage.author.tag, true)
-        .addField("Channel", newMessage.channel, true)
-        .addField("Old Message", oldMessage.content)
-        .addField("New Message", newMessage.content)
-        .setFooter(`Message ID: ${newMessage.id} | Author ID: ${newMessage.author.id}`);
-    
-    getModChannel(newMessage.guild).send({embed});
+    try
+    {
+        let embed = new Discord.RichEmbed()
+            .setTitle("**EDITED MESSAGE**")
+            .setColor("#fc3c3c")
+            .addField("Author", newMessage.author.tag, true)
+            .addField("Channel", newMessage.channel, true)
+            .addField("Old Message", oldMessage.content)
+            .addField("New Message", newMessage.content)
+            .setFooter(`Message ID: ${newMessage.id} | Author ID: ${newMessage.author.id}`);
+        
+        getModChannel(newMessage.guild).send({embed});
+    }
+    catch(e)
+    {
+        console.warn("ERROR: " + e);
+    }
 });
 
   // Ban Log
 client.on("guildBanAdd", function(guild, user){
-    let embed = new Discord.RichEmbed()
-        .setTitle("**USER BANNED**")
-        .setColor("#fc3c3c")
-        .addField("User", user.tag)
-        .setFooter(`User ID: ${user.id}`);
-    
-    getModChannel(guild).send({embed});
+    try
+    {
+        let embed = new Discord.RichEmbed()
+            .setTitle("**USER BANNED**")
+            .setColor("#fc3c3c")
+            .addField("User", user.tag)
+            .setFooter(`User ID: ${user.id}`);
+        
+        getModChannel(guild).send({embed});
+    }
+    catch(e)
+    {
+        console.warn("ERROR: " + e);
+    }
 });
 
   // Unban Log
 client.on("guildBanRemove", function(guild, user){
-    let embed = new Discord.RichEmbed()
+    try
+    {
+        let embed = new Discord.RichEmbed()
         .setTitle("**USER UNBANNED**")
         .setColor("#fc3c3c")
         .addField("User", user.tag)
         .setFooter(`User ID: ${user.id}`);
     
-    getModChannel(guild).send({embed});
+        getModChannel(guild).send({embed});
+    }
+    catch(e)
+    {
+        console.warn("ERROR: " + e);
+    }
 });

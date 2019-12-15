e636c32278bf5bb467381fe7f5041397d6161905
Zachary Damato <zacharyd1@live.com>
2019-12-15 00:17:47 -0500

    feat: Added a try/catch block around the server stats updater

--- modules/serverStats.ts
+++ modules/serverStats.ts
@@ -2,10 +2,17 @@ import {client} from "../Discord-Bot-Core/bot"
 
 async function updateMemberCount()
 {
-    let myGuild = client.guilds.get('538190725557518366');
-    let guildCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.get('604952771224928256');
-    memberCountChannel.setName('Members: ' + guildCount);
+    try
+    {
+        let myGuild = client.guilds.get('538190725557518366');
+        let guildCount = myGuild.memberCount;
+        let memberCountChannel = myGuild.channels.get('604952771224928256');
+        memberCountChannel.setName('Members: ' + guildCount);
+    }
+    catch(e)
+    {
+        console.log("Error: " + e)
+    }
 }
 
 //Member Count

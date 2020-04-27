bfee3213a6ddf3d82780bd300d9abe2f56605965
Zachary Damato <zacharyd1@live.com>
2020-04-27 02:47:02 -0400

    feat: updated purge.ts to fully use the built-in permissions system, and to not post errors to the chat

--- handlers/default/prefix/purge.ts
+++ handlers/default/prefix/purge.ts
@@ -10,18 +10,12 @@ module.exports = {
     // inChannelName: [],
     // inServerID: [],
     async execute(msg : Discord.Message, args : Array<string>) {
-          if (!msg.guild.me.hasPermission(["MANAGE_MESSAGES"]))
-          return msg.reply("Unable to purge, Lack of permission.");
-
-      let count : number = Number(args[0]);
-      
-      if (isNaN(count))
-          return msg.channel.send("**Please supply a valid number of messages to purge**");
-      if (count > 100)
-          return msg.channel.send("**Please supply a number less than 100**");
-          msg.channel.bulkDelete(1);
-          msg.channel
-          .bulkDelete(count)
-          .catch(error => msg.channel.send(`**ERROR:** ${error.message}`));
+        let count : number = Number(args[0]);
+        
+        if (isNaN(count)) return msg.channel.send("Please supply a valid number of messages to purge");
+        if (count > 100) return msg.channel.send("Please supply a number less than 100");
+            
+        await msg.channel.bulkDelete(count);
+        await msg.delete();
     }
 }

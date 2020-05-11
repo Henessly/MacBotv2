c88b980f29f5538bf021aba355df566eddd33618
Zachary Damato <zacharyd1@live.com>
2020-05-10 22:52:04 -0400

    feat: updated the member-count module to locate the target channel dynamically

--- modules/serverStats.ts
+++ modules/serverStats.ts
@@ -1,23 +1,31 @@
+import * as Discord from "discord.js";
+
 import {client} from "../Discord-Bot-Core/bot"
 
-async function updateMemberCount()
+async function updateMemberCount(target: Discord.Guild | Discord.GuildMember)
 {
-    return;
-    //TODO: we need to update this with the new target channel, if Mac still wants it
     try
     {
-        let myGuild = client.guilds.get('538190725557518366');
-        let guildCount = myGuild.memberCount;
-        let memberCountChannel = myGuild.channels.get('604952771224928256');
+        if(target instanceof Discord.GuildMember) {
+            target = target.guild;
+        }
+
+        let guildCount = target.memberCount;
+        let memberCountChannel = target.channels.find( ch => ch.name === "member-count");
+        if(!memberCountChannel) return;
         memberCountChannel.setName('Members: ' + guildCount);
     }
     catch(e)
     {
-        console.log("Error: " + e)
+        console.log(`Error updating member count: ${e}`);
     }
 }
 
 //Member Count
-client.on('ready', updateMemberCount);
+client.on('ready', () => {
+    client.guilds.forEach(guild => {
+        updateMemberCount(guild);
+    });
+});
 client.on('guildMemberRemove', updateMemberCount);
 client.on('guildMemberAdd', updateMemberCount);
